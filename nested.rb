e1603ec7b4e7bba01b4b89c5844e837151df7d69
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-15 21:11:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,10 +61,7 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+
 
 	programmer_hash = 
  		{
@@ -81,8 +78,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",
+    :languages => ["LISP", "C"]}
 end
 
 def changing_alan
